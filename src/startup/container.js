338f73d083ade17472612e4c22d901fa90c16576
Khay <altaircarn@gmail.com>
2020-01-30 14:59:41 -0600

    [ADD] controllers added

--- src/startup/container.js
+++ src/startup/container.js
@@ -13,7 +13,12 @@ const {
 } = require("../services/index");
 
 // Controllers
-const { HomeController } = require("../controllers");
+const {
+  HomeController,
+  UserController,
+  IdeaController,
+  CommentController
+} = require("../controllers");
 
 // Routes
 const { HomeRoutes } = require("../routes/index.routes");
@@ -44,7 +49,12 @@ container
     CommentService: asClass(CommentService).singleton()
   })
   .register({
-    HomeController: asClass(HomeController.bind(HomeController)).singleton()
+    HomeController: asClass(HomeController.bind(HomeController)).singleton(),
+    UserController: asClass(UserController.bind(UserController)).singleton(),
+    IdeaController: asClass(IdeaController.bind(IdeaController)).singleton(),
+    CommentController: asClass(
+      CommentController.bind(CommentController)
+    ).singleton()
   })
   .register({
     HomeRoutes: asFunction(HomeRoutes).singleton()
