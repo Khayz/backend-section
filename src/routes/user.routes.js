ba29596fad4871612690e7cef92e0b384b7d4ac0
Khay <altaircarn@gmail.com>
2020-01-30 19:18:18 -0600

    [ADD] pagination added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,9 @@
 const { Router } = require("express");
-const { AuthMiddleware } = require("../middlewares/index");
+const { AuthMiddleware, ParseIntMiddleware } = require("../middlewares/index");
 module.exports = ({ UserController }) => {
   const router = Router();
 
-  router.get("/", [AuthMiddleware], UserController.getAll);
+  router.get("/", [AuthMiddleware, ParseIntMiddleware], UserController.getAll);
   router.get("/:userId", UserController.get);
   router.patch("/:userId", UserController.update);
   router.delete("/:userId", UserController.delete);
